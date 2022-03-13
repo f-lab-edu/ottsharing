8f9d9b7ccbc5776d6fcb2c905342e6e7b3df7e30
deepredk <deepred.kim@gmail.com>
2022-03-13 17:12:12 +0900

    chore: 네이버 코딩컨벤션 설정

--- src/main/java/kr/flab/ottsharing/OttsharingApplication.java
+++ src/main/java/kr/flab/ottsharing/OttsharingApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OttsharingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OttsharingApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OttsharingApplication.class, args);
+    }
 
 }
