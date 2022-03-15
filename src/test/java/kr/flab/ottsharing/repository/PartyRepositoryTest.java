30dd0c1064f3547ec4f3b09ef382537d40415030
Deepred <deepred.kim@gmail.com>
2022-03-15 15:20:47 +0900

    feat: Party Repository 구현 및 Test
    근데 Test를 실제 디비 이용하도록 해놓은게 조금 걸려서
    테스트 시에는 h2 디비 이용하도록 변경해볼 예정

---
+++ src/test/java/kr/flab/ottsharing/repository/PartyRepositoryTest.java
@@ -0,0 +1,72 @@
+package kr.flab.ottsharing.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import kr.flab.ottsharing.entity.Party;
+import kr.flab.ottsharing.entity.User;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class PartyRepositoryTest {
+    @Autowired
+    private PartyRepository partyRepo;
+    @Autowired
+    private UserRepository userRepo;
+
+    @Test
+    void 파티_생성_및_조회_테스트() {
+        // given
+        final User user = User.builder().userId("user").build();
+        final User savedUser = userRepo.save(user);
+
+        final Party party = Party.builder().leader(savedUser).ottId("id").ottPassword("pass").build();
+
+        // when
+        partyRepo.save(party);
+
+        // then
+        Optional<Party> optionalParty = partyRepo.findById(party.getPartyId());
+        assertEquals(true, optionalParty.isPresent());
+
+        Party savedParty = optionalParty.get();
+        assertEquals("user", savedParty.getLeader().getUserId());
+        assertEquals("id", savedParty.getOttId());
+        assertEquals("pass", savedParty.getOttPassword());
+        assertEquals(false, savedParty.isFull());
+    }
+
+    @Test
+    void 빈_파티_목록_찾기_테스트() {
+        // given
+        final User user1 = User.builder().userId("user1").build();
+        final User savedUser1 = userRepo.save(user1);
+        final User user2 = User.builder().userId("user2").build();
+        final User savedUser2 = userRepo.save(user2);
+        final User user3 = User.builder().userId("user3").build();
+        final User savedUser3 = userRepo.save(user3);
+
+        final Party party1 = Party.builder().leader(savedUser1).ottId("id1").ottPassword("pass1").build();
+        final Party party2 = Party.builder().leader(savedUser2).ottId("id2").ottPassword("pass2").isFull(true).build();
+        final Party party3 = Party.builder().leader(savedUser3).ottId("id3").ottPassword("pass3").build();
+        partyRepo.save(party1);
+        partyRepo.save(party2);
+        partyRepo.save(party3);
+
+        // when
+        List<Party> notFullParties = (List<Party>) partyRepo.findByIsFullFalse();
+
+        // then
+        assertEquals(2, notFullParties.size());
+    }
+}
