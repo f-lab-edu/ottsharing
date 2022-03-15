6c403d7ba65345737c03b63c6d215179ef218e7c
Deepred <deepred.kim@gmail.com>
2022-03-15 15:36:38 +0900

    feat: PartyRepository 테스트 시 h2디비 사용하도록 함
    feat: PartyRepository 테스트 시 h2디비 사용하도록 함

--- src/test/java/kr/flab/ottsharing/repository/PartyRepositoryTest.java
+++ src/test/java/kr/flab/ottsharing/repository/PartyRepositoryTest.java
@@ -6,18 +6,18 @@ import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import kr.flab.ottsharing.entity.Party;
 import kr.flab.ottsharing.entity.User;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@Transactional
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+@TestPropertySource("classpath:application-test.yml")
 public class PartyRepositoryTest {
     @Autowired
     private PartyRepository partyRepo;
