42718c102360eefcf77c906c27dc669bddd06c95
Deepred <deepred.kim@gmail.com>
2022-03-17 15:44:01 +0900

    feat: PartyMember Repository 개량
    * PartyMemberRepository의 메소드를 써서 myParty API를 구현할 수가 없었음
    * 그래서 findPartyByUser, findUserByParty라는 메소드를 추가하는 개량을 함

--- src/main/java/kr/flab/ottsharing/repository/PartyMemberRepository.java
+++ src/main/java/kr/flab/ottsharing/repository/PartyMemberRepository.java
@@ -1,17 +1,24 @@
 package kr.flab.ottsharing.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import kr.flab.ottsharing.entity.Party;
 import kr.flab.ottsharing.entity.PartyMember;
 import kr.flab.ottsharing.entity.User;
 
+@Repository
 public interface PartyMemberRepository extends JpaRepository<PartyMember,Integer> {
 
-    List<PartyMember> findByUser(User user);
+    @Query("select pm.party from PartyMember pm where pm.user=:user")
+    Optional<Party> findPartyByUser(@Param("user") User user);
 
-
-
+    @Query("select pm.user from PartyMember pm where pm.party=:party")
+    List<User> findUsersByParty(@Param("party") Party party);
 
 }
