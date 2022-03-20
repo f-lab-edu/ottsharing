3f1cd7fb2674281a643c2421ec5393a66e5c6e1c
sue4869 <sue4869@naver.com>
2022-03-20 10:59:50 +0900

    refactor: UserRepository
    @Repository 추가

--- src/main/java/kr/flab/ottsharing/repository/UserRepository.java
+++ src/main/java/kr/flab/ottsharing/repository/UserRepository.java
@@ -6,7 +6,9 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import kr.flab.ottsharing.entity.User;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     boolean existsByUserId(String userId);
