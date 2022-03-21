63a58754da3b3ae3a5486240c0b328b758b230f0
deepredk <deepred.kim@gmail.com>
2022-03-21 17:11:48 +0900

    feat: PartyRepository 구조 개선
    * save 선언 삭제
    * findById의 매개변수명 명확하게 바꿈
    * findByIsFullTrue -> findNotFullOldestParties로 변경
    * 기존 요구사항은 가장 오래된 파티 '하나'만 가져오는 것이였으나 Hibernate 설계상 limit 키워드가 먹히지 않아서 불가피하게 List 사용함
    * 테스트케이스들도 구조 개선에 맞게 수정
    
    #31

--- src/main/java/kr/flab/ottsharing/repository/PartyRepository.java
+++ src/main/java/kr/flab/ottsharing/repository/PartyRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import kr.flab.ottsharing.entity.Party;
@@ -11,10 +12,8 @@ import kr.flab.ottsharing.entity.Party;
 @Repository
 public interface PartyRepository extends JpaRepository<Party,Integer> {
     @Override
-    <S extends Party> S save(S entity);
+    Optional<Party> findById(Integer partyId);
 
-    @Override
-    Optional<Party> findById(Integer integer);
-
-    Iterable<Party> findByIsFullFalse();
+    @Query(value = "select p from Party p where p.isFull = false order by p.createdTime asc")
+    List<Party> findNotFullOldestParties();
 }
