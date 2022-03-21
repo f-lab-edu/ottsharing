c9241aa5a838654c58386289560534f60407a67f
deepredk <deepred.kim@gmail.com>
2022-03-21 17:50:24 +0900

    feat: PartyMemberRepository 구조 개선
    * findOneByUser, findByParty로 개선
    * 개선한 코드에 맞게 테스트케이스 수정
    * 요구사항에서는 userId, partyId로 찾게했으나 그렇게 짜는 방법이 까다롭고, 보통 해당 엔티티(User, Party)를 얻은 뒤에나 이 메소드를 호출할 것이라고 생각되어 user, party로 찾게 했음

--- src/test/java/kr/flab/ottsharing/repository/PartyMemberRepositoryTest.java
+++ src/test/java/kr/flab/ottsharing/repository/PartyMemberRepositoryTest.java
@@ -1,16 +1,24 @@
 package kr.flab.ottsharing.repository;
 
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import kr.flab.ottsharing.entity.Party;
+import kr.flab.ottsharing.entity.PartyMember;
+import kr.flab.ottsharing.entity.User;
+
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
 @TestPropertySource("classpath:application-test.yml")
 public class PartyMemberRepositoryTest {
-
     @Autowired
     private PartyMemberRepository memberRepo;
     @Autowired
@@ -18,10 +26,8 @@ public class PartyMemberRepositoryTest {
     @Autowired
     private PartyRepository partyRepo;
 
-    // Party Entity 구조 변경으로 인해 동작이 안되는 코드
-    /*
     @Test
-    void 나의파티찾기_파티원목록조회_테스트(){
+    void User로_PartyMember_행_찾기_테스트(){
         //given
         final User user1 = User.builder().userId("유저1").build();
         final User savedUser1 = userRepo.save(user1);
@@ -30,23 +36,46 @@ public class PartyMemberRepositoryTest {
         final User user3 = User.builder().userId("유저3").build();
         final User savedUser3 = userRepo.save(user3);
 
-        final Party party = Party.builder().leader(savedUser1).ottId("id").ottPassword("pass").build();
+        final Party party = Party.builder().ottId("id").ottPassword("pass").build();
         final Party savedParty = partyRepo.save(party);
 
         //when
+        final PartyMember partyMember1 = PartyMember.builder().user(savedUser1).isLeader(true).party(savedParty).build();
+        memberRepo.save(partyMember1);
         final PartyMember partyMember2 = PartyMember.builder().user(savedUser2).party(savedParty).build();
         memberRepo.save(partyMember2);
         final PartyMember partyMember3 = PartyMember.builder().user(savedUser3).party(savedParty).build();
         memberRepo.save(partyMember3);
 
         //then
-        assertEquals(savedParty, memberRepo.findPartyByUserId(savedUser2).get());
-
-        List<User> members = memberRepo.findUsersByParty(savedParty);
-        assertEquals(true, members.contains(savedUser2));
-        assertEquals(true, members.contains(savedUser3));
+        assertEquals(partyMember2, memberRepo.findOneByUser(savedUser2).get());
     }
 
-     */
+    @Test
+    void Party로_PartyMember들_찾기_테스트() {
+        //given
+        final User user1 = User.builder().userId("유저1").build();
+        final User savedUser1 = userRepo.save(user1);
+        final User user2 = User.builder().userId("유저2").build();
+        final User savedUser2 = userRepo.save(user2);
+        final User user3 = User.builder().userId("유저3").build();
+        final User savedUser3 = userRepo.save(user3);
 
+        final Party party = Party.builder().ottId("id").ottPassword("pass").build();
+        final Party savedParty = partyRepo.save(party);
+
+        //when
+        final PartyMember partyMember1 = PartyMember.builder().user(savedUser1).isLeader(true).party(savedParty).build();
+        memberRepo.save(partyMember1);
+        final PartyMember partyMember2 = PartyMember.builder().user(savedUser2).party(savedParty).build();
+        memberRepo.save(partyMember2);
+        final PartyMember partyMember3 = PartyMember.builder().user(savedUser3).party(savedParty).build();
+        memberRepo.save(partyMember3);
+
+        //then
+        List<PartyMember> members = memberRepo.findByParty(savedParty);
+        assertEquals(true, members.contains(partyMember1));
+        assertEquals(true, members.contains(partyMember2));
+        assertEquals(true, members.contains(partyMember3));
+    }
 }
