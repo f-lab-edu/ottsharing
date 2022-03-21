b5343758d1b6a528262a58127ac754c3a0a018af
deepredk <deepred.kim@gmail.com>
2022-03-21 21:49:55 +0900

    feat: PartyWaitingRepository 개선
    * save, deleteById 삭제 (이미 구현된 것은 추가 선언 안하기로 정했기때문)
    * 최종적으로 existsByUser, deleteByUser, findTop3ByorderByCreatedTimeAsc, existsBy만 남김
    * 사용할 Repository 메서드들에 대한 테스트케이스 작성

--- src/main/java/kr/flab/ottsharing/repository/PartyWaitingRepository.java
+++ src/main/java/kr/flab/ottsharing/repository/PartyWaitingRepository.java
@@ -9,15 +9,11 @@ import kr.flab.ottsharing.entity.PartyWaiting;
 
 @Repository
 public interface PartyWaitingRepository extends JpaRepository<PartyWaiting, Integer> {
-    @Override
-    <S extends PartyWaiting> S save(S entity);
-
     boolean existsByUser(User user);
 
-    Iterable<PartyWaiting> findTop3ByOrderByCreatedTimeAsc();
+    void deleteByUser(User user);
 
-    @Override
-    void deleteById(Integer id);
+    Iterable<PartyWaiting> findTop3ByOrderByCreatedTimeAsc();
 
     boolean existsBy();
 }
