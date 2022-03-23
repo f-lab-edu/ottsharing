03856f5c71e963b2c9070ad0ee4dca16b2a4ac44
deepredk <deepred.kim@gmail.com>
2022-03-23 17:05:03 +0900

    feat: UserService fetchMyInfo 구현
    * login이 된 자신의 아이디만 넣어 호출한다는 전제하에 작성하였음
    * 추후 login 구현됐을때 현재 로그인 된 아이디로 호출하게끔 수정 필요

--- src/main/java/kr/flab/ottsharing/service/UserService.java
+++ src/main/java/kr/flab/ottsharing/service/UserService.java
@@ -3,6 +3,7 @@ package kr.flab.ottsharing.service;
 import org.springframework.stereotype.Service;
 
 import kr.flab.ottsharing.entity.User;
+import kr.flab.ottsharing.protocol.MyInfo;
 import kr.flab.ottsharing.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 
@@ -24,12 +25,22 @@ public class UserService {
         return null;
     }
 
+    // User Entity 구조 변경으로 인해 동작하지 않는 코드
     public User enrollUser(String loginId){
+        /*
         User user = User.builder()
             .userId(loginId)
             .build();
 
         User savedUser = userRepository.save(user);
         return savedUser;
+        */
+    }
+
+    // login이 된 자신의 아이디만 넣어 호출한다는 전제하에 작성하였음
+    // 추후 login 구현됐을때 현재 로그인 된 아이디로 호출하게끔 수정 필요
+    public MyInfo fetchMyInfo(String userId) {
+        User user = userRepository.findByUserId(userId);
+        return new MyInfo(userId, user.getEmail(), user.getMoney());
     }
 }
