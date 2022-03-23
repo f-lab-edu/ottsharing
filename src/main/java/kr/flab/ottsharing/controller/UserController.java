11ba171fd1a7a74c453c12cd55215c76e1a562e7
deepredk <deepred.kim@gmail.com>
2022-03-23 17:43:15 +0900

    feat: UserController DELETE /myPage 구현
    * userId는 "user"로 고정해놨음
    * 추후 JWT login 구현되면 현재 로그인 된 아이디 가져오도록 수정

--- src/main/java/kr/flab/ottsharing/controller/UserController.java
+++ src/main/java/kr/flab/ottsharing/controller/UserController.java
@@ -5,11 +5,13 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import kr.flab.ottsharing.entity.User;
+import kr.flab.ottsharing.protocol.UserDeleteResult;
 import kr.flab.ottsharing.service.UserService;
 import lombok.RequiredArgsConstructor;
 
@@ -17,14 +19,14 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class UserController {
 
-    private final UserService loginService;
+    private final UserService userService;
 
     @PostMapping("/login")
     public String IdCheck(@RequestParam String userId,HttpServletResponse response){
 
-        User loginMember = loginService.loginCheck(userId);
+        User loginMember = userService.loginCheck(userId);
         if(loginMember == null){
-            loginMember = loginService.enrollUser(userId);
+            loginMember = userService.enrollUser(userId);
         }
 
         Cookie cookie = new Cookie("memberId",String.valueOf(loginMember.getUserId()));
@@ -43,4 +45,9 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
+    @DeleteMapping("/myPage")
+    public UserDeleteResult leave() {
+        String userId = "user"; // 추후 JWT login 구현되면 현재 로그인 된 아이디 가져오도록 수정
+        return userService.deleteMyInfo(userId);
+    }
 }
