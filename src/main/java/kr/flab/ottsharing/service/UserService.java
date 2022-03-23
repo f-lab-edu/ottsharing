238bd8ccc10d0d1e5272dd179dea58a0de656fe0
deepredk <deepred.kim@gmail.com>
2022-03-23 17:36:40 +0900

    feat: UserService deleteMyInfo 구현
    * 돈이 0원이고, 가입된 파티가 없어야 탈퇴되도록 함
    * userId는 로그인 된 아이디를 컨트롤러에서 넘겨준다고 전제함
    * JWT login이 구현되면 추후 수정

--- src/main/java/kr/flab/ottsharing/service/UserService.java
+++ src/main/java/kr/flab/ottsharing/service/UserService.java
@@ -3,6 +3,8 @@ package kr.flab.ottsharing.service;
 import org.springframework.stereotype.Service;
 
 import kr.flab.ottsharing.entity.User;
+import kr.flab.ottsharing.protocol.UserDeleteResult;
+import kr.flab.ottsharing.repository.PartyMemberRepository;
 import kr.flab.ottsharing.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 
@@ -11,6 +13,7 @@ import lombok.RequiredArgsConstructor;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PartyMemberRepository partyMemberRepository;
 
 
     // User Repository 구조 변경으로 인해 동작하지 않는 코드
@@ -24,12 +27,31 @@ public class UserService {
         return null;
     }
 
+    // User Entity 구조 변경으로 인해 동작하지 않는 코드
     public User enrollUser(String loginId){
+        /*
         User user = User.builder()
             .userId(loginId)
             .build();
 
         User savedUser = userRepository.save(user);
         return savedUser;
+        */
+        return null;
+    }
+
+    // userId는 로그인 된 아이디를 컨트롤러에서 넘겨준다고 전제함
+    // JWT /login이 구현되면 추후 수정
+    public UserDeleteResult deleteMyInfo(String userId) {
+        User user = userRepository.findByUserId(userId);
+        if (user.getMoney() > 0) {
+            return UserDeleteResult.HAS_MONEY;
+        }
+        if (partyMemberRepository.findOneByUser(user).isPresent()) {
+            return UserDeleteResult.HAS_PARTY;
+        }
+
+        userRepository.deleteById(user.getId());
+        return UserDeleteResult.SUCCESS;
     }
 }
