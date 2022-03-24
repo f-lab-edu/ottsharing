08130f45fd7a1c99380a4845524c4a0f09117021
sue4869 <sue4869@naver.com>
2022-03-24 18:37:29 +0900

    fix: 깃충돌로 인하여 다시 올림

--- src/main/java/kr/flab/ottsharing/controller/UserController.java
+++ src/main/java/kr/flab/ottsharing/controller/UserController.java
@@ -3,18 +3,30 @@ package kr.flab.ottsharing.controller;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 
+import kr.flab.ottsharing.protocol.MyPageUpdateResult;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import kr.flab.ottsharing.entity.User;
 import kr.flab.ottsharing.service.UserService;
 import lombok.RequiredArgsConstructor;
 
+import java.util.Map;
+
 @RestController
 @RequiredArgsConstructor
 public class UserController {
 
+    public final UserService userService;
+
+    @PutMapping("/myPage")
+    public MyPageUpdateResult changeMyInfo(@RequestBody Map<String, String> request) {
+        String userId = request.get("userId");
+        String password = request.get("password");
+        String email = request.get("email");
+
+        return userService.updateMyInfo(userId, password, email);
+
+    }
 }
