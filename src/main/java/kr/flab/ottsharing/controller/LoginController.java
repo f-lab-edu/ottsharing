d1b5d83d95f0d9fd97f7f7115ea0f26c7cad77e7
sumin <68679529+sue4869@users.noreply.github.com>
2022-03-24 17:58:33 +0900

    Revert "LoginService, LoginController -> UserService, UserController 이름 변경 #89"

--- src/main/java/kr/flab/ottsharing/controller/LoginController.java
+++ src/main/java/kr/flab/ottsharing/controller/LoginController.java
@@ -1,23 +1,31 @@
 package kr.flab.ottsharing.controller;
 
+import java.util.Optional;
+
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import kr.flab.ottsharing.entity.User;
-import kr.flab.ottsharing.service.UserService;
+import kr.flab.ottsharing.service.LoginService;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
-public class UserController {
+public class LoginController {
 
-    private final UserService loginService;
+    private final LoginService loginService;
 
     @PostMapping("/login")
     public String IdCheck(@RequestParam String userId,HttpServletResponse response){
