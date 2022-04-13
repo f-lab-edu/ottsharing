81b840d64752b89e8f17d19d1312c836e3223ac1
sue4869 <sue4869@naver.com>
2022-04-14 00:02:19 +0900

    feat: 파티 해체 및 리더확인 기능 구현
    * 리더 확인 메소드 구현- 관련 항목 : PartyMemberService
    * 파티 해체 api
    - 파티멤버들 삭제 후 파티 완전 삭제하도록 함
    - 파티의 리더만 권한 부여
    - 리더가 속한 파티인지 확인 후 해체
    - Exception 기능 추가
    - postman으로 테스트 진행
    - 관련 항목
    PartyController, PartyService -> api 기능 구현
    PartyMemberRepository, PartyRepository -> delete 쿼리 추가
    application.yml -> deleteMapping 이용을 위한 조치 추가
    WrongInfoException -> RuntimeException을 확장함. 조건에 해당되지 않을 경우 예외발생

---
+++ src/main/java/kr/flag/ottsharing/exception/WrongInfoException.java
@@ -0,0 +1,8 @@
+package kr.flag.ottsharing.exception;
+
+public class WrongInfoException extends RuntimeException{
+
+    public WrongInfoException(String message) {
+        super(message);
+    }
+}
