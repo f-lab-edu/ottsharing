e16d9250ee3aa3a6804b9f9aa14a09a63751350c
sue4869 <sue4869@naver.com>
2022-04-19 16:26:27 +0900

    feat : 충전 api 기능 구현
    * postman 테스트 완료

---
+++ src/main/java/kr/flab/ottsharing/controller/MoneyController.java
@@ -0,0 +1,22 @@
+package kr.flab.ottsharing.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import kr.flab.ottsharing.service.MoneyService;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class MoneyController {
+
+    private final MoneyService moneyService;
+
+    @PostMapping("/charge")
+    public String chargeMoney(@RequestParam String userId, @RequestParam int moneyToCharge) {
+     
+        return moneyService.charge(userId, moneyToCharge);
+    }
+    
+}
