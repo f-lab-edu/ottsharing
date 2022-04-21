6ae3b32ba3b64fac32c91d38380b472c8207b4b5
Deepred <deepred.kim@gmail.com>
2022-04-21 15:13:32 +0900

    feat: 매일 아침 6시, 정산/자동결제 실행

--- src/main/java/kr/flab/ottsharing/OttsharingApplication.java
+++ src/main/java/kr/flab/ottsharing/OttsharingApplication.java
@@ -2,8 +2,10 @@ package kr.flab.ottsharing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class OttsharingApplication {
 
     public static void main(String[] args) {
