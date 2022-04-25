bbe7371be878bec556f991fac5b40d0437427bb5
Deepred <deepred.kim@gmail.com>
2022-04-25 10:08:24 +0900

    refactor: RequestDTO 적용 - Money

--- src/main/java/kr/flab/ottsharing/controller/MoneyController.java
+++ src/main/java/kr/flab/ottsharing/controller/MoneyController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import kr.flab.ottsharing.dto.request.MoneyDto;
 import kr.flab.ottsharing.service.MoneyService;
 import lombok.RequiredArgsConstructor;
 
@@ -15,13 +17,13 @@ public class MoneyController {
     private final MoneyService moneyService;
 
     @PostMapping("/charge")
-    public String chargeMoney(@CookieValue(name = "userId") String userId, @RequestParam int moneyToCharge) {
-        return moneyService.charge(userId, moneyToCharge);
+    public String chargeMoney(@CookieValue(name = "userId") String userId, @RequestBody MoneyDto chargeDto) {
+        return moneyService.charge(userId, chargeDto.getAmount());
     }
 
     @PostMapping("/withdraw")
-    public String withdrawMoney(@CookieValue(name = "userId") String userId, @RequestParam int moneyToWithdraw) {
-        return moneyService.withdraw(userId, moneyToWithdraw);
+    public String withdrawMoney(@CookieValue(name = "userId") String userId, @RequestBody MoneyDto withdrawDto) {
+        return moneyService.withdraw(userId, withdrawDto.getAmount());
     }
     
 }
