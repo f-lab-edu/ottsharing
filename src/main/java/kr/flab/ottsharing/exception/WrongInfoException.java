58ba0a53f48ddbad6a608877858dee1f21b4d7e7
sue4869 <sue4869@naver.com>
2022-04-29 00:08:42 +0900

    refator: PR때 빌더 문제로 지웠던 파일 다시 복구

---
+++ src/main/java/kr/flab/ottsharing/exception/WrongInfoException.java
@@ -0,0 +1,8 @@
+package kr.flab.ottsharing.exception;
+
+public class WrongInfoException extends RuntimeException {
+
+    public WrongInfoException(String message) {
+        super(message);
+    }
+}
